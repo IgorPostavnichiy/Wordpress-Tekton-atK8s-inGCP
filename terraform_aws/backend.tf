ca0146c82610b3903eecf55e1033f5884a66e7f0
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-06-20 19:24:33 +0300

    add project files

--- terraform_aws/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mondyk8sbucket2023"  
-    key            = "terraform.tfstate"   
-    region         = "us-east-1"           
-    encrypt        = true                  
-    dynamodb_table = "terraform-lock" 
-  }
-}
