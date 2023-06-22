9f30bb0fdf8abe6bfef05594a953e54497a32dad
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-06-22 11:52:47 +0300

    destroy

--- terraform_gcp/backend.tf
+++ terraform_gcp/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "gcs" {
-    bucket = "igorbucketgcp" # Replace with you bucket name. +
+    bucket = "igorbucketgcp" # Replace with you bucket name.
     prefix = "terraform/state"
     credentials = "credentials_file.json"
   }
 }
+
+
--- terraform_gcp/variables.tf
+++ terraform_gcp/variables.tf
@@ -1,27 +1,27 @@
 variable "def_name" {
-  default = "igorpostspout" # Replace a name for your resources. +
+  default = "igorpostspout" # Replace a name for your resources.
 }
 
 variable "project_id" {}
 
 variable "region_prj" {
-  default = "us-central1" # Replace with you region. -
+  default = "us-central1" # Replace with you region.
 }
 
 variable "zone_prj" {
-  default = "us-central1-a" # Replace with you zone. -
+  default = "us-central1-a" # Replace with you zone.
 }
 
 variable "sa_name" {
-  default = "my-test-account-name@my-it-sprout-project.iam.gserviceaccount.com" # Replace with you name for tfstate. +
+  default = "my-test-account-name@my-it-sprout-project.iam.gserviceaccount.com" # Replace with you name for tfstate.
 }
 
 variable "sa_account" {
-  default = "my-test-account-name@my-it-sprout-project.iam.gserviceaccount.com" # Replace with you service account. +
+  default = "my-test-account-name@my-it-sprout-project.iam.gserviceaccount.com" # Replace with you service account.
 }
 
 variable "domain" {
-  default = "it-sproutdevteam.fun" # Replace with your domain. +
+  default = "it-sproutdevteam.fun" # Replace with your domain.
 }
 variable "local_ip_range" {
   default = "10.100.0.0/28"
